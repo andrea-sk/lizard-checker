8e36bf58de40be73a8d09c526c4446f3de0ff843
andrea-sk <andrea.dodet@mail-bip.com>
2021-03-23 15:43:00 +0100

    tests: refactor tests

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,51 +1,53 @@
-import os
 import glob
-from checker import check_assert
-from checker import run_checks
+from argparse import Namespace
+import os
 import utils
+import checker
+import checker_cli
 
 
-def test_single_check(single_expr, good_avro, bad_avro):
-    # True with good input
-    good_res = check_assert(single_expr, good_avro)
-    assert good_res
+def test_run_passing_checks(good_avro, config):
+    feed_class = checker.DifOutput(good_avro)
 
-    # False with bad input
-    bad_res = check_assert(single_expr, bad_avro)
-    assert not bad_res
+    for check in config["common_checks"]:
+        feed_class.check_expr(check)
+
+    # Check if the counter was updated accordingly
+    assert feed_class.failed == 0
 
 
-def test_run_passing_checks(monkeypatch, config, good_avro):
+def test_run_failing_checks(bad_avro, config):
+    feed_class = checker.DifOutput(bad_avro)
+
+    for check in config["common_checks"]:
+        feed_class.check_expr(check)
+
+    # Check if the counter was updated accordingly
+    assert feed_class.failed > 0
+
+
+def test_cli_runner(monkeypatch, config, good_avro):
+    def mocked_get_args():
+        return Namespace(
+            avro_path="some_avros/", checks_file="fake_checks.json", export_json=False
+        )
+
     def mock_glob_glob(path):
-        res = [
-            os.path.join("my_avro_files", "gb-dcm-user-stream-eviction-2021-03-16.avro")
-        ]
+        res = [os.path.join("my_avro_files", "fake_file.avro")]
         return res
 
-    def mock_read_avro(file_path):
+    def mock_read_checks(path):
+        return config
+
+    def mock_read_avro(fpath):
         return good_avro
 
-    monkeypatch.setattr(glob, "glob", mock_glob_glob)
+    monkeypatch.setattr(checker_cli, "get_args", mocked_get_args)
+    monkeypatch.setattr(utils, "read_checks", mock_read_checks)
     monkeypatch.setattr(utils, "read_avro", mock_read_avro)
-
-    res_good = run_checks("fake_folder", config)
-
-    assert res_good["passed"] == 10 and res_good["failed"] == 0
-
-
-def test_run_failing_checks(monkeypatch, config, bad_avro):
-    def mock_glob_glob(path):
-        res = [
-            os.path.join("my_avro_files", "gb-dcm-user-stream-eviction-2021-03-16.avro")
-        ]
-        return res
-
-    def mock_read_avro(file_path):
-        return bad_avro
-
     monkeypatch.setattr(glob, "glob", mock_glob_glob)
-    monkeypatch.setattr(utils, "read_avro", mock_read_avro)
 
-    res_bad = run_checks("fake_folder", config)
+    res = checker_cli.main()
 
-    assert res_bad["failed"] != 0
+    assert res["passed"] == 6
+    assert res["failed"] == 0
